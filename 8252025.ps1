41edb9429e4c763bd3d3f79242cae101312c0ba5
pope-17 <karl.rodriguez@pfda.gov.ph>
2025-08-25 15:40:51 +0800

    Add files via upload

---
+++ 8252025.ps1
@@ -0,0 +1,10 @@
+﻿Implementition Data Confidentialy
+using Public keys:
+@coreBABA:
+config t
+ip domain-name pfda.gov.ph
+crypto key generate rsa
+!! tanong, anong KeySize: 256.512 1024
+ip ssh version 2
+ip ssh time-out 120
+end
